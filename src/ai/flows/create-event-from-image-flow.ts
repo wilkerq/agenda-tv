48c7707a900d2e75cf3e5ae0a18affea8856b6ac
Wilker Quirino <wilkerq@gmail.com>
2025-09-11 03:06:58 +0000

    a solucao nao foi definitiva e continua o erro

--- src/ai/flows/create-event-from-image-flow.ts
+++ src/ai/flows/create-event-from-image-flow.ts
@@ -25,7 +25,7 @@ export async function createEventFromImage(input: CreateEventFromImageInput): Pr
 
 const prompt = ai.definePrompt({
     name: 'createEventFromImagePrompt',
-    model: 'googleai/gemini-pro-vision',
+    model: 'googleai/gemini-pro',
     input: { schema: CreateEventFromImageInputSchema },
     output: { schema: CreateEventFromImageOutputSchema },
     tools: [getEventsForDay],
@@ -97,4 +97,3 @@ const createEventFromImageFlow = ai.defineFlow(
         return output!;
     }
 );
-
