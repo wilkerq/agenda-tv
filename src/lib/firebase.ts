140c52a2bf96269fb9159702f10a3cdb2a7fde90
Wilker Quirino <wilkerq@gmail.com>
2025-09-22 17:56:27 +0000

    const firebaseConfig = {   apiKey: "AIzaSyAiHptzLoHCtDR2_n9pPVbD89ewX8kd

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,15 +4,14 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
-// ATENÇÃO: Substitua o objeto abaixo pela configuração do seu projeto Firebase.
-// Você pode encontrá-lo no Firebase Console > Configurações do Projeto.
+// Configuração do seu projeto Firebase.
 const firebaseConfig = {
-  "projectId": "agenda-alego-v3-72653978-2d924",
-  "appId": "1:547317231231:web:b965cb0dc294adaba3194a",
-  "storageBucket": "agenda-alego-v3-72653978-2d924.firebasestorage.app",
-  "apiKey": "AIzaSyAiHptzLoHCtDR2_n9pPVbD89ewX8kd_f8",
-  "authDomain": "agenda-alego-v3-72653978-2d924.firebaseapp.com",
-  "messagingSenderId": "547317231231"
+  apiKey: "AIzaSyAiHptzLoHCtDR2_n9pPVbD89ewX8kd_f8",
+  authDomain: "agenda-alego-v3-72653978-2d924.firebaseapp.com",
+  projectId: "agenda-alego-v3-72653978-2d924",
+  storageBucket: "agenda-alego-v3-72653978-2d924.firebasestorage.app",
+  messagingSenderId: "547317231231",
+  appId: "1:547317231231:web:b965cb0dc294adaba3194a"
 };
 
 
