2b1eaa8684969285ca7b28d6b161c5568f4e24c9
Wilker Quirino <wilkerq@gmail.com>
2025-10-30 00:42:10 +0000

    File changes

--- src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "agenda-news-room-4491522-e400a",
-  "appId": "1:363314732473:web:de90e196a1961d1c646a68",
-  "apiKey": "AIzaSyAvMXEWfRiGaZfMf3LLdvbG0waaXEbh4Mg",
-  "authDomain": "agenda-news-room-4491522-e400a.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "363314732473"
-};
