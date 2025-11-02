5bfdc5905bbc0b7c28494db9acf6d0cea015a1a4
Wilker Quirino <wilkerq@gmail.com>
2025-11-02 14:22:05 +0000

    const firebaseConfig = {   apiKey: "AIzaSyAvMXEWfRiGaZfMf3LLdvbG0waaXEbh

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,10 @@
 
   export const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    apiKey: "AIzaSyAvMXEWfRiGaZfMf3LLdvbG0waaXEbh4Mg",
+    authDomain: "agenda-news-room-4491522-e400a.firebaseapp.com",
+    projectId: "agenda-news-room-4491522-e400a",
+    storageBucket: "agenda-news-room-4491522-e400a.firebasestorage.app",
+    messagingSenderId: "363314732473",
+    appId: "1:363314732473:web:de90e196a1961d1c646a68"
   };
+
