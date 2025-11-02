6b6f00ab4aad73348d0f832b136f6d613d04584b
Wilker Quirino <wilkerq@gmail.com>
2025-11-02 11:51:44 +0000

    digo pra ver por causa desse erro! deveria pegar dentro do arquivo .env

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 
 import type {NextConfig} from 'next';
+import 'dotenv/config';
 
 const nextConfig: NextConfig = {
   /* config options here */
--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 
   export const firebaseConfig = {
-    apiKey: "AIzaSyDGkB7kxb8oK39K-G_1J4weRoqxMQYKRhA",
-    authDomain: "agenda-alego-v3-72653978-39fba.firebaseapp.com",
-    projectId: "agenda-alego-v3-72653978-39fba",
-    storageBucket: "agenda-alego-v3-72653978-39fba.appspot.com",
-    messagingSenderId: "814315709282",
-    appId: "1:814315709282:web:fbc64b7c698c68cde6823b"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   };
