03b464cdf8985ac484aec37f8347a4dc53fe982b
Wilker Quirino <wilkerq@gmail.com>
2025-11-26 17:58:31 +0000

    certeza que ta tudo pronto?

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,18 +1,22 @@
+// This file is intentionally left blank.
+// It is used by Firebase Messaging to handle background notifications.
+// The service worker will be automatically generated and registered by Firebase.
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here, other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/10.9.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.9.0/firebase-messaging-compat.js');
 
-// Scripts for firebase and firebase messaging
-importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
-
-// Initialize the Firebase app in the service worker
-// "Default" Firebase app is used for initialization
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// You can get this config from the Firebase console.
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID",
-    measurementId: "YOUR_MEASUREMENT_ID"
+  "projectId": "agenda-news-room-4491522-e400a",
+  "appId": "1:363314732473:web:de90e196a1961d1c646a68",
+  "storageBucket": "agenda-news-room-4491522-e400a.firebasestorage.app",
+  "apiKey": "AIzaSyAvMXEWfRiGaZfMf3LLdvbG0waaXEbh4Mg",
+  "authDomain": "agenda-news-room-4491522-e400a.firebaseapp.com",
+  "messagingSenderId": "363314732473"
 };
 
 firebase.initializeApp(firebaseConfig);
